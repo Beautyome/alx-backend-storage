7d991e8465116eae920e503494155423f5d10b39
Beautyome <beautyupeleomo@gmail.com>
2024-01-24 03:14:42 -0800

    rdis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,35 +4,33 @@ import redis
 from uuid import uuid4
 from typing import Union, Callable, Optional
 from functools import wraps
+
 UnionOfTypes = Union[str, bytes, int, float]
 
-
 def count_calls(method: Callable) -> Callable:
-    """ decorator Counts how many times methods of Cache class are called """
+    """ Decorator: Counts how many times methods of Cache class are called """
     key = method.__qualname__
 
     @wraps(method)
     def wrapper(self, *args, **kwds):
-        """ This is wrapper function for call_calls method """
+        """ Wrapper function for count_calls method """
         self._redis.incr(key)
         return method(self, *args, **kwds)
     return wrapper
 
-
 def call_history(method: Callable) -> Callable:
-    """ Stores the history of inputs and outputs for a particular function """
+    """ Decorator: Stores the history of inputs and outputs for a particular function """
     input_list = method.__qualname__ + ":inputs"
     output_list = method.__qualname__ + ":outputs"
 
     @wraps(method)
     def wrapper(self, *args) -> bytes:
-        """ This is wrapper function for call_history method """
+        """ Wrapper function for call_history method """
         self._redis.rpush(input_list, str(args))
         output = method(self, *args)
         self._redis.rpush(output_list, output)
         return output
     return wrapper
-
 
 class Cache:
     """ Class for methods that operate a caching system """
@@ -45,18 +43,16 @@ class Cache:
     @count_calls
     @call_history
     def store(self, data: UnionOfTypes) -> str:
-    """
-    Method takes a data argument and returns a string
-    Generate a random key (e.g. using uuid), store the input data in Redis
-    using the random key and return the key
-    """
-    key = str(uuid4())
-    self._redis.set(key, data)
-    return key
+        """
+        Method takes a data argument and returns a string
+        Generate a random key (e.g. using uuid), store the input data in Redis
+        using the random key and return the key
+        """
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
 
-    def get(self,
-            key: str,
-            fn: Optional[Callable] = None) -> UnionOfTypes:
+    def get(self, key: str, fn: Optional[Callable] = None) -> UnionOfTypes:
         """
         Retrieves data stored at a key
         converts the data back to the desired format
@@ -65,9 +61,21 @@ class Cache:
         return fn(data) if fn else data
 
     def get_str(self, key: str) -> str:
-        """ get a string """
+        """ Get a string """
         return self.get(key, str)
 
     def get_int(self, key: str) -> int:
-        """ get an int """
+        """ Get an int """
         return self.get(key, int)
+
+def replay(cache: "Cache"):
+    """
+    Display the history of calls of a particular function
+    """
+    method_name = cache.store.__qualname__
+    inputs = cache._redis.lrange(f"{method_name}:inputs", 0, -1)
+    outputs = cache._redis.lrange(f"{method_name}:outputs", 0, -1)
+
+    print(f"{method_name} was called {len(inputs)} times:")
+    for inp, out in zip(inputs, outputs):
+        print(f"{method_name}(*{inp.decode('utf-8')}) -> {out.decode('utf-8')}")
